3e6ab9eab4ecdf3b23c756c3b4515e243c721927
Zhz2025 <Zhz2025@github.com>
2025-05-01 23:50:01 +0800

    test commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/OpMode2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/OpMode2025.java
@@ -26,8 +26,7 @@ import org.firstinspires.ftc.teamcode.messages.PoseMessage;
 @TeleOp
 public class OpMode2025 extends LinearOpMode {
 
-    //zhzdaociyiyou
-    //xiexie
+
     FlightRecorder recorder;
     GoBildaPinpointDriver odo;
     MecanumDrive drive;
