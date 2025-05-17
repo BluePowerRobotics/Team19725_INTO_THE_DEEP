3b344489528341a2444e377addcd4a66a9629f43
Zhz2025 <Zhanghz270140@163.com>
2025-05-17 10:50:24 +0800

    zhz Commit (2025/05/17) add easyclimb

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/EasyClimb.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class EasyClimb {
+    double climbLeftLength;
+    double climbRightLength;
+    boolean climbUp;
+    double t =System.currentTimeMillis();
+    HardwareMap hardwareMap;
+    DcMotor climbLeft, climbRight;
+    Gamepad gamepad2;
+    public void initClimb(HardwareMap hardwareMaprc, Gamepad gamepad2rc){
+        hardwareMap = hardwareMaprc;
+        climbLeft = hardwareMap.get(DcMotor.class,"climbLeft");
+        climbRight = hardwareMap.get(DcMotor.class,"climbRight");
+        climbLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        climbRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        t = System.currentTimeMillis();
+        gamepad2 =gamepad2rc;
+        climbLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        climbRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        while (System.currentTimeMillis() - t <= 2000) {
+            climbLeft.setPower(1);
+            climbRight.setPower(1);
+        }
+    }
+    void climb() {
+        if (gamepad2.left_bumper) {
+            climbLeft.setPower(-1);
+            climbRight.setPower(-1);
+        } else if(gamepad2.right_bumper){
+            climbLeft.setPower(1);
+            climbRight.setPower(1);
+        }
+    }
+}
