2e50516a2ba18afcb4a09b7cbc2de9a5c957b6c9
BluePower-jdfz <ftc19725@outlook.com>
2025-05-25 01:10:27 +0800

    Updated a new controller for a five-servo arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArmController.java
@@ -16,36 +16,21 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class ArmController {
-    private Blinker control_Hub;
-    private Blinker expansion_Hub_2;
-    private HardwareDevice webcam_1;
-    public DcMotor armMotor;
-    //private DcMotor armPuller;
-    private  DcMotor armSpinner;
-    private Gyroscope eimu;
-    private IMU imu;
-    private DcMotor leftBack;
-    private DcMotor leftFront;
-    private DcMotor rightBack;
-    private DcMotor rightFront;
-    private Servo servoe0;
-    private Servo servoe1;
-    private Servo servoe2;
-    private Servo servoe3;
-    public Servo servoe4;
-    private Servo servoe5;
-    private Servo servos0;
-    private Servo servos1;
-    private Servo servos2;
-    private Servo servos3;
-    private Servo servos4;
-    private Servo servos5;
+    private Servo inTakeLengthController;
+    private Servo inTakePositionController;
+    private Servo clipController;
     private HardwareMap hardwaremap;
     private Gamepad gamepad1;
     private Gamepad gamepad2;
-    private DistanceSensor distanceSensor;
+
+    private double clipLockPos;
+    private double clipUnlockPos;
 
 
+
+
+
+    public boolean AUTO = false;
 
     public MODE RUNMODE = MODE.HIGH_CHAMBER;
     public enum MODE {HIGH_CHAMBER,LOW_CHAMBER,HIGH_BASKET,LOW_BASKET}
@@ -97,16 +82,64 @@ public class ArmController {
             if(inTakeStatus!= INTAKE_MODE.WAITING)inTakeStatus = chamberInTake();
         }
     }
+    private boolean inTakeActionInitiated=false, inTakeActionFinished = false;
+
     public INTAKE_MODE chamberInTake(){
         if(INTAKE_RUNMODE==INTAKE_MODE.EXTENDING){
+            if(!inTakeActionInitiated){
 
+                inTakeActionInitiated = true;
+            }
+
+            if(AUTO&&inTakeActionFinished){
+                INTAKE_RUNMODE = INTAKE_MODE.TAKING;
+                inTakeActionInitiated = false;
+            }
         } else if (INTAKE_RUNMODE==INTAKE_MODE.TAKING) {
+            if(!inTakeActionInitiated){
 
+                inTakeActionInitiated = true;
+            }
+
+            if(AUTO&&inTakeActionFinished){
+                INTAKE_RUNMODE = INTAKE_MODE.SHORTENING;
+                inTakeActionInitiated = false;
+            }
         } else if(INTAKE_RUNMODE==INTAKE_MODE.SHORTENING){
+            if(!inTakeActionInitiated){
 
+                inTakeActionInitiated = true;
+            }
+
+            if(AUTO&&inTakeActionFinished){
+                INTAKE_RUNMODE = INTAKE_MODE.PUTTING;
+                inTakeActionInitiated = false;
+            }
         } else if (INTAKE_RUNMODE==INTAKE_MODE.PUTTING) {
+            if(!inTakeActionInitiated){
 
+                inTakeActionInitiated = true;
+            }
+
+            if(AUTO&&inTakeActionFinished){
+                INTAKE_RUNMODE = INTAKE_MODE.WAITING;
+                inTakeActionInitiated = false;
+            }
         }
         return INTAKE_RUNMODE;
     }
+    public double armALength=0;
+    public double armBLength=0;
+    public double inTakeMinLength=0;
+    public double inTakeTargetLength;
+    public double inTakeTargetLengthAngle;
+    public double inTakeTargetAngle;
+    public void setInTakePosition(double inTakeLength,double inTakeAngle){
+        inTakeTargetLength = inTakeLength;
+        inTakeTargetAngle = inTakeAngle;
+    }
+    public void inTakeLengthCalculator(){
+        inTakeTargetLengthAngle = Math.acos((armALength*armALength+(inTakeTargetLength+inTakeMinLength)*(inTakeTargetLength+inTakeMinLength)-armBLength*armBLength)/(2*armALength*(inTakeTargetLength+inTakeMinLength)))/Math.PI;
+    }
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/FiveServosArmController.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class FiveServosArmController {
+    HardwareMap hardwareMap;
+    Telemetry telemetry;
+    Servo servoA,servoB,servoC,servoD,servoE,servoF;
+    double lengthA,lengthB,lengthC,positionA,positionB;
+    double clipLockPos,clipUnlockPos;
+    public void initArm(HardwareMap hardwareMapRC, Telemetry telemetryRC){
+        hardwareMap = hardwareMapRC;
+        telemetry = telemetryRC;
+        servoA = hardwareMap.get(Servo.class,"");
+        servoB = hardwareMap.get(Servo.class,"");
+        servoC = hardwareMap.get(Servo.class,"");
+        servoD = hardwareMap.get(Servo.class,"");
+        servoE = hardwareMap.get(Servo.class,"");
+        servoF = hardwareMap.get(Servo.class,"");
+        servoA.setDirection(Servo.Direction.REVERSE);//逆时针
+        servoB.setDirection(Servo.Direction.REVERSE);//逆时针
+        servoC.setDirection(Servo.Direction.FORWARD);//顺时针
+        servoD.setDirection(Servo.Direction.FORWARD);//顺时针
+        servoE.setDirection(Servo.Direction.FORWARD);
+        servoF.setDirection(Servo.Direction.FORWARD);//夹取
+    }
+
+    //均为向上正方向，向右正方向，向前正方向
+    //D为顺时针
+    public void setLocation(double x,double y,double z,double radianArmPosition,double radianClipPosition) {
+        double lengthAC;
+        double length;
+        double alphaDegree=0;
+        alphaDegree = Math.toDegrees(Math.atan(Math.abs(y / x)));
+        if (y <= 0 && x > 0)
+            alphaDegree = 360 - alphaDegree;
+        if (y <= 0 && x <= 0)
+            alphaDegree = 180 + alphaDegree;
+        if (y > 0 && x <= 0)
+            alphaDegree = 180 - alphaDegree;
+        servoA.setPosition(alphaDegree/270);
+        double r = Math.sqrt(x*x+y*y);
+        length = Math.sqrt(r*r+z*z);
+        lengthAC = Math.sqrt(length * length + lengthC * lengthC - 2 * length * lengthC * Math.cos(Math.toRadians(Math.abs(alphaDegree))-0.5*Math.PI+radianArmPosition));
+        double radianA = Math.acos((lengthA*lengthA+lengthAC*lengthAC-lengthB*lengthB)/(2*lengthA*lengthAC));
+        double radianB = Math.acos((lengthA*lengthA+lengthB*lengthB-lengthAC*lengthAC)/(2*lengthA*lengthB));
+        double radianC = 2*Math.PI-radianA-radianB;
+        double radianCRest = Math.acos((lengthAC*lengthAC+lengthC*lengthC-length*length)/(2*lengthAC*lengthC));
+        servoD.setPosition((radianC+radianCRest)/(0.75*Math.PI));
+        servoC.setPosition(radianB/(0.75*Math.PI));
+        double radianARest = Math.PI*0.5-radianArmPosition-radianC-radianCRest;
+        servoB.setPosition((radianA+radianARest)/(0.75*Math.PI));
+    }
+    public void setClip(boolean lock){
+        if(lock) servoF.setPosition(clipLockPos);
+        else servoF.setPosition(clipUnlockPos);
+    }
+}
