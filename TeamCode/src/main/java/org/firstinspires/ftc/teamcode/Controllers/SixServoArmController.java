42f5dfb9fed2ecb91cbbedd2b3e360b62908fa6f
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-06-02 21:29:16 +0800

    Update SixServoArmController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/SixServoArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/SixServoArmController.java
@@ -18,6 +18,10 @@ public class SixServoArmController {
     private static final double clipLockPos=0,clipUnlockPos=0;
     private final int[] servoDegree={270,180,180,270,180,180};
     private double[] servoTargetDegree;
+    private double[] servoNowDegree;
+    private double[] servoDegreeError;
+    private double[] servoSpeed={0.24,0.24,0.24,0.24,0.24};//sec per 60 degree
+    private double servoRunTimeMax =0;
     private final double[] servoZeroPositionDegree={0,0,0,0,0};
 
     //private final int servoADegree = 270,servoBDegree = 180,servoCDegree = 180,servoDDegree = 270,servoEDegree = 180,servoFDegree = 180;
@@ -54,44 +58,51 @@ public class SixServoArmController {
         this.radianArmPosition = radianArmPosition;
         this.radianClipPosition = radianClipPosition;
         setLocationTime=System.currentTimeMillis();
+        double lengthAC;
+        double length;
+        double alphaDegree=0;
+
+        if (x == 0 && y > 0)
+            alphaDegree = 90;
+        else if (y <= 0 && x == 0)
+            alphaDegree = 270;
+        else {
+            double degrees = Math.toDegrees(Math.atan(Math.abs(y / x)));
+            if (y <= 0 && x > 0)
+                alphaDegree = 360 - degrees;
+            else if (y <= 0 && x < 0)
+                alphaDegree = 180 + degrees;
+            else if (y > 0 && x < 0)
+                alphaDegree = 180 - degrees;
+        }
+
+
+        double r = Math.sqrt(x*x+y*y);
+        length = Math.sqrt(r*r+z*z);
+        lengthAC = Math.sqrt(length * length + lengthC * lengthC - 2 * length * lengthC * Math.cos(Math.toRadians(Math.abs(alphaDegree))-0.5*Math.PI+radianArmPosition));
+        double radianA = Math.acos((lengthA*lengthA+lengthAC*lengthAC-lengthB*lengthB)/(2*lengthA*lengthAC));
+        double radianB = Math.acos((lengthA*lengthA+lengthB*lengthB-lengthAC*lengthAC)/(2*lengthA*lengthB));
+        double radianC = 2*Math.PI-radianA-radianB;
+        double radianCRest = Math.acos((lengthAC*lengthAC+lengthC*lengthC-length*length)/(2*lengthAC*lengthC));
+        double radianARest = Math.PI*0.5-radianArmPosition-radianC-radianCRest;
+        servoTargetDegree[0]=alphaDegree;
+        servoTargetDegree[1]=Math.toDegrees(radianA+radianARest);
+        servoTargetDegree[2]=Math.toDegrees(radianB);
+        servoTargetDegree[3]=Math.toDegrees(radianC+radianCRest);
+        servoTargetDegree[4]=Math.toDegrees(radianClipPosition);
+        for(int i = 0;i<=4;i++){
+            servoDegreeError[i] = servoTargetDegree[i]-servoNowDegree[i];
+            servoRunTimeMax = Math.max(servoRunTimeMax,servoSpeed[i]*60*servoDegreeError[i]);
+        }
+
 
     }
+
     public void setMode(SIX_SERVO_ARM_RUNMODE SIX_SERVO_ARM_MODE){
         this.SIX_SERVO_ARM_MODE = SIX_SERVO_ARM_MODE;
         switch (this.SIX_SERVO_ARM_MODE) {
             case RUN_TO_POSITION: {
-                double lengthAC;
-                double length;
-                double alphaDegree=0;
-
-                if (x == 0 && y > 0)
-                    alphaDegree = 90;
-                else if (y <= 0 && x == 0)
-                    alphaDegree = 270;
-                else {
-                    double degrees = Math.toDegrees(Math.atan(Math.abs(y / x)));
-                    if (y <= 0 && x > 0)
-                        alphaDegree = 360 - degrees;
-                    else if (y <= 0 && x < 0)
-                        alphaDegree = 180 + degrees;
-                    else if (y > 0 && x < 0)
-                        alphaDegree = 180 - degrees;
-                }
-
-
-                double r = Math.sqrt(x*x+y*y);
-                length = Math.sqrt(r*r+z*z);
-                lengthAC = Math.sqrt(length * length + lengthC * lengthC - 2 * length * lengthC * Math.cos(Math.toRadians(Math.abs(alphaDegree))-0.5*Math.PI+radianArmPosition));
-                double radianA = Math.acos((lengthA*lengthA+lengthAC*lengthAC-lengthB*lengthB)/(2*lengthA*lengthAC));
-                double radianB = Math.acos((lengthA*lengthA+lengthB*lengthB-lengthAC*lengthAC)/(2*lengthA*lengthB));
-                double radianC = 2*Math.PI-radianA-radianB;
-                double radianCRest = Math.acos((lengthAC*lengthAC+lengthC*lengthC-length*length)/(2*lengthAC*lengthC));
-                double radianARest = Math.PI*0.5-radianArmPosition-radianC-radianCRest;
-                servoTargetDegree[0]=alphaDegree;
-                servoTargetDegree[1]=Math.toDegrees(radianA+radianARest);
-                servoTargetDegree[2]=Math.toDegrees(radianB);
-                servoTargetDegree[3]=Math.toDegrees(radianC+radianCRest);
-                servoTargetDegree[4]=Math.toDegrees(radianClipPosition);
+                
 //        servoPosition[0]=alphaDegree/servoDegree[0];
 //        servoPosition[1]=(radianA+radianARest)/Math.toRadians(servoDegree[1]);
 //        servoPosition[2]=radianB/Math.toRadians(servoDegree[2]);
@@ -104,8 +115,15 @@ public class SixServoArmController {
                     servo[i].setPosition(servoPosition[i]);
                 }
                 break;
-            }case STOP_AND_RESET:
+            }case STOP_AND_RESET: {
+                setTargetLocation(resetX,resetY,resetZ,0.5*Math.PI,0.5*Math.PI);
+                for(int i = 0;i<=4;i++){
+                    servoPosition[i] = (servoTargetDegree[i]-servoZeroPositionDegree[i])/servoDegree[i];
+                    servoPosition[i]=Math.min(1,Math.max(0,servoPosition[i]));
+                    servo[i].setPosition(servoPosition[i]);
+                }
                 break;
+            }
         }
     }
     public void setClip(boolean lock){
