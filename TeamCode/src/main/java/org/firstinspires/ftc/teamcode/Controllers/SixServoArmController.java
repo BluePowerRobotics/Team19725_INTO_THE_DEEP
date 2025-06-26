9154afeac6fbdaf2e67379748cec8ba72631ab78
WilliamGao1130 <gaoyiwei1130@outlook.com>
2025-06-26 18:55:20 +0800

    Refactor SixServoArmController to update RUNMODE enum and improve target position handling; enhance telemetry updates for better servo control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/SixServoArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/SixServoArmController.java
@@ -1,14 +1,12 @@
 package org.firstinspires.ftc.teamcode.Controllers;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import java.text.MessageFormat;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 
 class ServoRadianCalculator {
@@ -24,7 +22,6 @@ class ServoRadianCalculator {
         else if (x>0 && y<0){theta = Math.PI*2-Math.atan(y/x);}
         else if (x==0 && y>0){theta = Math.PI*0.5;}
         else if (x==0 && y<0){theta = Math.PI*1.5;}
-        else {theta = 0;}
         double length1 = x-c*Math.sin(alpha4)*Math.cos(theta);
         double length2 = y-c*Math.sin(alpha4)*Math.sin(theta);
         double length3 = z+c*Math.cos(alpha4);
@@ -128,7 +125,7 @@ public class SixServoArmController {
     private HardwareMap hardwareMap;
     private Telemetry telemetry;
     public enum SIX_SERVO_ARM_RUNMODE{
-        RUN_TO_POSITION,STOP_AND_RESET,RUN_WITHOUT_PREDICTOR
+        RUN_TO_POSITION,STOP_AND_RESET, RUN_WITHOUT_LOCATOR
     }
     private SIX_SERVO_ARM_RUNMODE SIX_SERVO_ARM_MODE;
     private long setLocationTime;
@@ -139,34 +136,46 @@ public class SixServoArmController {
         servoValueOutputter = new ServoValueOutputter(hardwareMap, telemetry, servoRadianCalculator);
     }
     public void initArm(){
-        setMode(SIX_SERVO_ARM_RUNMODE.RUN_WITHOUT_PREDICTOR);
+        setMode(SIX_SERVO_ARM_RUNMODE.RUN_WITHOUT_LOCATOR);
         setTargetPosition(resetX, resetY, resetZ, 0 * Math.PI, 0.5 * Math.PI);
     }
 
     final double resetX = 100,resetY = 0,resetZ = 10;
     double targetX = resetX,targetY = resetY,targetZ = resetZ;
     double nowX=resetX,nowY=resetY,nowZ=resetZ;
+    double recentX = resetX,recentY = resetY,recentZ = resetZ;
     double Distance;
     double servoMoveTime = 0.0;//单位秒
-    private double[] servoSpeed={0.24,0.24,0.24,0.24,0.24};//sec per 60 degree
+    private double[] servoSpeed={0.36,0.24,0.24,0.24,0.24};//sec per 60 degree
     //均为向上正方向，向右正方向，向前正方向
     //D为顺时针
     double targetClipRadian = 0.5 * Math.PI;
     public void setTargetPosition(double X,double Y,double Z,double alpha4,double clipRadian) {
-        targetX = X;
-        targetY = Y;
-        targetZ = Z;
-        targetClipRadian = clipRadian;
-        setLocationTime = System.currentTimeMillis();
-        Distance = Math.sqrt((X-nowX)*(X-nowX)+(Y-nowY)*(Y-nowY)+(Z-nowZ)*(Z-nowZ));
-        double[] targetPosition = servoRadianCalculator.calculate(targetX, targetY, targetZ, alpha4);
-        double[] nowPosition = servoRadianCalculator.calculate(nowX, nowY, nowZ, targetClipRadian);
-        double[] servoMoveTime = new double[4];
-        for(int i = 0; i <= 3; i++) {
-            servoMoveTime[i] = Math.toDegrees(targetPosition[i] - nowPosition[i])*(servoSpeed[i]/60.0);
+        switch (SIX_SERVO_ARM_MODE) {
+            case RUN_TO_POSITION:
+                targetX = X;
+                targetY = Y;
+                targetZ = Z;
+                targetClipRadian = clipRadian;
+                setLocationTime = System.currentTimeMillis();
+                Distance = Math.sqrt((X-nowX)*(X-nowX)+(Y-nowY)*(Y-nowY)+(Z-nowZ)*(Z-nowZ));
+                double[] targetPosition = servoRadianCalculator.calculate(targetX, targetY, targetZ, alpha4);
+                double[] nowPosition = servoRadianCalculator.calculate(nowX, nowY, nowZ, targetClipRadian);
+                double[] servoMoveTime = new double[4];
+                for(int i = 0; i <= 3; i++) {
+                    servoMoveTime[i] = Math.toDegrees(targetPosition[i] - nowPosition[i])*(servoSpeed[i]/60.0);
+                }
+                double servoMoveTimeMax = Arrays.stream(servoMoveTime).max().getAsDouble();
+                this.servoMoveTime = servoMoveTimeMax;
+                recentX = nowX;
+                recentY = nowY;
+                recentZ = nowZ;
+                break;
+            case RUN_WITHOUT_LOCATOR:
+                servoValueOutputter.setRadians(servoRadianCalculator.calculate(targetX, targetY, targetZ, targetClipRadian), targetClipRadian, true);
+                break;
         }
-        double servoMoveTimeMax = Arrays.stream(servoMoveTime).max().getAsDouble();
-        this.servoMoveTime = servoMoveTimeMax;
+
     }
 
     public void setMode(SIX_SERVO_ARM_RUNMODE mode) {
@@ -174,12 +183,23 @@ public class SixServoArmController {
         switch(SIX_SERVO_ARM_MODE){
             case RUN_TO_POSITION:
                 telemetry.addData("SixServoArmController","RUN_TO_POSITION");
+                if (System.currentTimeMillis() - setLocationTime >= servoMoveTime * 1000) {
+                    nowX = targetX;
+                    nowY = targetY;
+                    nowZ = targetZ;
+                } else {
+                    double ratio = (System.currentTimeMillis() - setLocationTime) / (servoMoveTime * 1000);
+                    nowX = recentX + (targetX - recentX) * ratio;
+                    nowY = recentY + (targetY - recentY) * ratio;
+                    nowZ = recentZ + (targetZ - recentZ) * ratio;
+                }
+                servoValueOutputter.setRadians(servoRadianCalculator.calculate(nowX, nowY, nowZ, targetClipRadian), targetClipRadian, true);
                 break;
             case STOP_AND_RESET:
                 telemetry.addData("SixServoArmController","STOP_AND_RESET");
-                servoValueOutputter.setRadians(servoRadianCalculator.calculate(resetX, resetY, resetZ, 0.5 * Math.PI),targetClipRadian, true);
+                servoValueOutputter.setRadians(servoRadianCalculator.calculate(resetX, resetY, resetZ, 0.5 * Math.PI),0.5*Math.PI, true);
                 break;
-            case RUN_WITHOUT_PREDICTOR:
+            case RUN_WITHOUT_LOCATOR:
                 telemetry.addData("SixServoArmController","RUN_WITHOUT_PREDICTOR");
                 break;
         }
