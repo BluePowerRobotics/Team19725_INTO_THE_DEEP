8a1ea1ec5e54e907cc0506e503315ed49410d7bd
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-07-03 11:18:24 +0800

    Update IntakeLengthController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/IntakeLengthController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/IntakeLengthController.java
@@ -3,34 +3,23 @@ package org.firstinspires.ftc.teamcode.Controllers;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class BasicArmController {
+public class IntakeLengthController {
     private Servo inTakeLengthController;
-    private Servo inTakePositionController;
-    private Servo clipController;
-    private double clipLockPos;
-    private double clipUnlockPos;
     private HardwareMap hardwaremap;
     public void initBasicArm(HardwareMap hardwaremap){
         this.hardwaremap = hardwaremap;
         inTakeLengthController = this.hardwaremap.get(Servo.class,"");
-        inTakePositionController = this.hardwaremap.get(Servo.class,"");
     }
     private double armALength=0;
     private double armBLength=0;
     private double inTakeMinLength=0;
     private double inTakeTargetLength;
     private double inTakeTargetLengthAngle;
-    private double inTakeTargetAngle;
-    public void setInTakePosition(double inTakeLength,double inTakeAngle){
+    public void setInTakePosition(double inTakeLength){
         inTakeTargetLength = inTakeLength;
-        inTakeTargetAngle = inTakeAngle;
-    }
-    public void inTakeLengthCalculator(){
         inTakeTargetLengthAngle = Math.acos((armALength*armALength+(inTakeTargetLength+inTakeMinLength)*(inTakeTargetLength+inTakeMinLength)-armBLength*armBLength)/(2*armALength*(inTakeTargetLength+inTakeMinLength)))/Math.PI;
-
     }
-    public void setInTake(){
+    public void setLength(){
         inTakeLengthController.setPosition(inTakeTargetLengthAngle);
-        inTakePositionController.setPosition(inTakeTargetAngle);
     }
 }
