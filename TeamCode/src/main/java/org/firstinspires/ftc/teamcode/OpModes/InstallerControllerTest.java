72c99806131e1fa330fe8433783a330df1dece4a
JoshuaGuo23 <2975749084@qq.com>
2025-07-09 16:11:38 +0800

    gybcommitinstallercontroller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/InstallerControllerTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.roadrunner.SequentialAction;
+import org.firstinspires.ftc.teamcode.Controllers.InstallerController;
+@TeleOp
+
+public class InstallerControllerTest extends LinearOpMode {
+    public void runOpMode() {
+        InstallerController installerController = new InstallerController(hardwareMap, gamepad1, gamepad2, telemetry);
+        waitForStart();
+        Actions.runBlocking(
+
+                new SequentialAction(
+                        installerController.installerPuller(),
+                        installerController.beamSpinner(false)
+                )
+        );
+    }
+}
