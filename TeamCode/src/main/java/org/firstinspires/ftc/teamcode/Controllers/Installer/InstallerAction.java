d869cc7f2e17da2f5452540c81761b7b46a8d5b5
JoshuaGuo23 <2975749084@qq.com>
2025-07-17 17:24:30 +0800

    717 gyb update InstallerAction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Installer/InstallerAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Installer/InstallerAction.java
@@ -14,11 +14,18 @@ import org.firstinspires.ftc.teamcode.Controllers.RobotStates.INSTALL_RUNMODE;
 
 public class InstallerAction {
     public boolean isUpping = false;
+    public static double Beam_low = 0.15;//与installer对接
+    public static double Beam_mid = 0.16666666666;//从观察区取clip
+    public static double Beam_high = 0.35;//提起clip，使之脱离观察区樯
     double InstallPos = 233;
     double SixthClipInstallPos = 58;
     double ClipLength = 25;
     double SpitDis = 40;
     int CurrentNum = 1;
+
+
+    boolean IFstartBacking = false;
+    double WaitStartTime = 0;
     HardwareMap hardwareMap;
     Gamepad gamepad1;
     Gamepad gamepad2;
@@ -90,7 +97,7 @@ public class InstallerAction {
             while (CurrentNum <= 6) {
                 if (state == 0) {
                     ClipPosition = SixthClipInstallPos + (6 - CurrentNum) * ClipLength;
-                    clipInstallPuller.setPosition(1);
+                    clipInstallPuller.setPosition(0);
                     UpstartTime = System.currentTimeMillis();
                     state = 1;
                     telemetry.addData("Puller", "Moving to clip: " + CurrentNum);
@@ -98,6 +105,7 @@ public class InstallerAction {
                 }
                 if (state == 1) {
                     if (dis <= ClipPosition ||
+                            //todo 更改时间
                             System.currentTimeMillis() - UpstartTime > 2000) {
 
                         clipInstallPuller.setPosition(0.5);
@@ -107,20 +115,41 @@ public class InstallerAction {
                     return true;
                 }
                 if (state == 2) {
-                    clipInstallPuller.setPosition(0.5);
+                    clipInstallPuller.setPosition(1);
                     if (dis >= InstallPos ||
                             System.currentTimeMillis() - UpstartTime > 2000 * 2) {
 
                         state = 3;
                         telemetry.addData("Puller", "Returned to center");
+                        return false;
                     }
-                    return true;
                 }
+                telemetry.addData("Clip Install Puller", "Pulling clip " + CurrentNum);
+                telemetry.update();
+            }
+            return true;
+        }
+    }
+    public Action installerPuller() {
+        // Create an action to pull the installer
+        return new InstallerPuller();
+    }
+    public class SpitClip implements Action{
+        private boolean initialized = false;
+        int state = 0;
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            double dis = disSensor.getDis();
+            if (!initialized) {
+                clipInstallPuller.setPosition(0);
+                initialized = true;
+            }
+            while (CurrentNum <= 6) {
                 if (state == 3) {
                     clipInstallPuller.setPosition(1);
-                    if (dis <= InstallPos - SpitDis ||
-                            System.currentTimeMillis() - UpstartTime > 2000 * 3) {
-                        clipInstallPuller.setPosition(0);
+                    if (dis >= InstallPos + SpitDis) {
+                        clipInstallPuller.setPosition(0.5);
+                        WaitStartTime = System.currentTimeMillis();
                         state = 4;
                         telemetry.addData("Puller", "Spit specimen");
                     }
@@ -128,24 +157,29 @@ public class InstallerAction {
                 }
                 if (state == 4) {
                     clipInstallPuller.setPosition(0.5);
-                    if (dis >= InstallPos ||
-                            System.currentTimeMillis() - UpstartTime > 2000 * 4) {
+                    if (System.currentTimeMillis() - WaitStartTime > 500){
+                        IFstartBacking = true;
+                        clipInstallPuller.setPosition(0);
+                    }
+                    if (dis <= InstallPos && IFstartBacking) {
                         clipInstallPuller.setPosition(0.5);
                         telemetry.addData("Puller", "Final center");
+                        CurrentNum++;
+                        IFstartBacking = false;
+                        state = 0;
                         return false;
                     }
                     return true;
                 }
                 telemetry.addData("Clip Install Puller", "Pulling clip " + CurrentNum);
                 telemetry.update();
-                CurrentNum++;
             }
             return false;
         }
     }
-    public Action installerPuller() {
+    public Action spitClip() {
         // Create an action to pull the installer
-        return new InstallerPuller();
+        return new SpitClip();
     }
     long UpStartTime = 0;
     public class BeamSpinner implements Action{
@@ -157,72 +191,74 @@ public class InstallerAction {
         public boolean run(@NonNull TelemetryPacket packet) {
             if (ifdown) {
                 isUpping = false;
-                beamSpinner.setPosition(0.08);
+                beamSpinner.setPosition(Beam_mid);
+                return false;
             }
             else {
-                beamSpinner.setPosition(0.3);
+                beamSpinner.setPosition(Beam_high);
                 if(!isUpping){
                     UpStartTime = System.currentTimeMillis();
                     isUpping = true;
                 }
                 if(System.currentTimeMillis() - UpStartTime > 300 && System.currentTimeMillis() - UpStartTime < 1000){
-                    beamSpinner.setPosition(0.10);
+                    beamSpinner.setPosition(Beam_low);
+                    return false;
                 }
             }
             telemetry.addData("Beam Spinner", beamSpinner.getPosition());
             telemetry.update();
-            return false;
+            return System.currentTimeMillis() - UpStartTime <= 300 || System.currentTimeMillis() - UpStartTime >= 1000;
         }
     }
     public Action beamSpinner(boolean ifdown) {
         return new BeamSpinner(ifdown);
     }
-    public void setMode(INSTALL_RUNMODE installStates) {
-        // Set the installation mode to the specified run mode
-        this.installStates = installStates;
-        switch (this.installStates) {
-            case WAITING:
-                PrepareInited = false;
-                clipInstallPuller.setPosition(0.5);
-                clipInstaller.setPosition(0);
-                break;
-            case EATING:
-                clipInstaller.setPosition(0);
-                if(!PrepareInited) {
-                    // Perform any necessary preparation tasks here
-                    // For example, initializing components or setting up configurations
-                    telemetry.addData("Installer", "Preparing for installation");
-                    telemetry.update();
-                    prepareStartTime = System.currentTimeMillis();
-                    PrepareInited = true;
-                }
-                if(System.currentTimeMillis()-prepareStartTime < 1000) {
-                    clipInstallPuller.setPosition(0);
-                    telemetry.addData("Installer", "Preparing");
-                    telemetry.update();
-                }
-                break;
-            case INSTALLING:
-                PrepareInited = false;
-                clipInstallPuller.setPosition(0.5);
-
-                if(!InstallInited) {
-                    // Perform any necessary preparation tasks here
-                    // For examp le, initializing components or setting up configurations
-                    telemetry.addData("Installer", "install init");
-                    telemetry.update();
-                    installStartTime = System.currentTimeMillis();
-                    InstallInited = true;
-                }
-                if(System.currentTimeMillis()-installStartTime < 1000) {
-                    clipInstaller.setPosition(0.8);
-                    telemetry.addData("Installer", "Installing");
-                    telemetry.update();
-                }else{
-                    clipInstaller.setPosition(0);
-                    telemetry.addData("Installer", "Installation complete");
-                }
-                break;
-        }
-    }
+//    public void setMode(INSTALL_RUNMODE installStates) {
+//        // Set the installation mode to the specified run mode
+//        this.installStates = installStates;
+//        switch (this.installStates) {
+//            case WAITING:
+//                PrepareInited = false;
+//                clipInstallPuller.setPosition(0.5);
+//                clipInstaller.setPosition(0);
+//                break;
+//            case EATING:
+//                clipInstaller.setPosition(0);
+//                if(!PrepareInited) {
+//                    // Perform any necessary preparation tasks here
+//                    // For example, initializing components or setting up configurations
+//                    telemetry.addData("Installer", "Preparing for installation");
+//                    telemetry.update();
+//                    prepareStartTime = System.currentTimeMillis();
+//                    PrepareInited = true;
+//                }
+//                if(System.currentTimeMillis()-prepareStartTime < 1000) {
+//                    clipInstallPuller.setPosition(0);
+//                    telemetry.addData("Installer", "Preparing");
+//                    telemetry.update();
+//                }
+//                break;
+//            case INSTALLING:
+//                PrepareInited = false;
+//                clipInstallPuller.setPosition(0.5);
+//
+//                if(!InstallInited) {
+//                    // Perform any necessary preparation tasks here
+//                    // For examp le, initializing components or setting up configurations
+//                    telemetry.addData("Installer", "install init");
+//                    telemetry.update();
+//                    installStartTime = System.currentTimeMillis();
+//                    InstallInited = true;
+//                }
+//                if(System.currentTimeMillis()-installStartTime < 1000) {
+//                    clipInstaller.setPosition(0.8);
+//                    telemetry.addData("Installer", "Installing");
+//                    telemetry.update();
+//                }else{
+//                    clipInstaller.setPosition(0);
+//                    telemetry.addData("Installer", "Installation complete");
+//                }
+//                break;
+//        }
+//    }
 }
