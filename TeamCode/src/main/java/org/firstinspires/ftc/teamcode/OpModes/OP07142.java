3476dfa5c698e0979d807585b83fbd162d780241
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-07-23 03:26:26 +0800

    add so many control method according to the gamePad map; change the ways to control the chassis to make it same everywhere; fix keyboard conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/OP07142.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/OP07142.java
@@ -10,7 +10,10 @@ import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Controllers.ChassisController;
+import org.firstinspires.ftc.teamcode.Controllers.Installer.InstallerController;
 import org.firstinspires.ftc.teamcode.Controllers.IntakeLength.*;
+import org.firstinspires.ftc.teamcode.Controllers.OutPut.OutputController;
+import org.firstinspires.ftc.teamcode.Controllers.RobotStates;
 import org.firstinspires.ftc.teamcode.Controllers.SixServoArm.ServoValueEasyOutputter;
 import org.firstinspires.ftc.teamcode.Controllers.SixServoArm.SixServoArmEasyController;
 import org.firstinspires.ftc.teamcode.Vision.FindCandidate;
@@ -52,8 +55,8 @@ public class OP07142 extends LinearOpMode{
     public boolean xhasbeenpressed = false;
 
     public boolean ahasbeenpressed = false, lbhasbeenpressed = false, rbhasbeenpressed = false;
-    public int servo_select = 3;
-    public double servo_position = 0.5;
+    //public int servo_select = 3;
+    //public double servo_position = 0.5;
     // servos5:0.86~1
     //ArmDirectController armDirectController = new ArmDirectController();
     ChassisController chassisController = new ChassisController();
@@ -63,6 +66,8 @@ public class OP07142 extends LinearOpMode{
     private void inithardware() {
 
         CVModule.init(hardwareMap, telemetry , 0);
+        outputController = new OutputController(hardwareMap);
+        installerController = new InstallerController(hardwareMap,gamepad1,gamepad2,telemetry);
         intakeLengthController = MotorLineIntakeLengthController.getInstance(hardwareMap);
         sixServoArmController=SixServoArmEasyController.getInstance(hardwareMap,telemetry);
         servoValueEasyOutputter=sixServoArmController.servoValueOutputter;
@@ -97,6 +102,7 @@ public class OP07142 extends LinearOpMode{
         orientation = imu.getRobotYawPitchRollAngles();
         imu.resetYaw();
         updateOrientation();
+        sixServoArmController.initArm();
 
     }
 
@@ -147,26 +153,16 @@ public class OP07142 extends LinearOpMode{
             move_y_l = gamepad1.left_stick_y;
             move_x_r = gamepad1.right_stick_x;
             move_y_r = gamepad1.right_stick_y;
-            chassisController.chassisController(move_x_l, -move_x_r, move_y_l + move_y_r);
+            chassisController.chassisController(move_x_r, -move_y_l,-move_x_l);
 
             // moveselect(move_x_l,-move_x_r,move_y_l+move_y_r);
             // moveselect(move_x_l,-move_y_l-move_y_r,-move_x_r);
             fps_and_tele();
 
             degree = 0.0036984 * orientation.getYaw(AngleUnit.DEGREES) + 0.499286;
-            motorcontrol();
-            servocontrol();
-
-            if(gamepad1.left_bumper){
-                ArmAction Command = CVModule.CalculateAverage(CVModule);
-                if(Command.suggestion != -2 && !HasSetArmPos){
-                    x=Command.GoToX-94;
-                    y=-Command.GoToY+44;
-                    clipRadian = Command.ClipAngle;
-                    HasSetArmPos = true;
-                }
-
-            }
+            motorControl();
+            servoControl();
+            outputAndInstallerControl();
 
 
             /*
@@ -180,68 +176,236 @@ public class OP07142 extends LinearOpMode{
         }
     }
 
-    boolean bhasbeenpressed = false, cliplock = false;
-    public void motorcontrol(){
-        if(gamepad2.x){
-            intake.setPower(0.5);
-        }else if(gamepad2.y){
-            intake.setPower(-0.5);
-
+    //boolean bhasbeenpressed = false, cliplock = false;
+    boolean requiresForce=false;
+    double installRequiresForcePower=0.3;
+    public void motorControl(){
+        if(gamepad2.left_trigger > 0.1){
+            //intakeLengthController.SingleMotorControl(gamepad2.left_trigger);
+            intakeLengthController.SingleMotorControl(gamepad2.left_trigger);//放出滑轨
+        }
+        else if(gamepad2.right_trigger > 0.1){
+            intakeLengthController.SingleMotorControl(-gamepad2.right_trigger);//收回滑轨
+        }else if(requiresForce){
+            intakeLengthController.SingleMotorControl(-installRequiresForcePower);
         }else{
-            intake.setPower(0);
+            intakeLengthController.SingleMotorControl(0);
         }
-        if(gamepad2.a){
-            output.setPower(0.5);
-        }else if(gamepad2.b){
-            output.setPower(-0.5);
 
-        }else{
-            output.setPower(0);
-        }
-        if(gamepad2.left_bumper){
-            intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
-        if(gamepad2.right_bumper){
-            output.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            output.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
+
+
     }
-    double x=-109.4778,y=149.6528;
-    double clipRadian = 1.473;
+    double x=0 ,y=150;
+    double clipRadian = 0;
     boolean leftstickbuttonhasbeenpressed= false;
     boolean locked=false;
-    public static double kx=2;
-    public static double ky=2;
-    public void servocontrol() {
-        x+=gamepad2.left_stick_x*kx;
-        y-=gamepad2.left_stick_y*ky;
-        if(!gamepad2.left_bumper)
-            sixServoArmController.setTargetPosition(x,y,Math.PI,clipRadian).update();
-//        else
-//            servoValueEasyOutputter.giveTheSample();
-        ServoValueEasyOutputter.ClipPosition pos;
-        if(gamepad2.left_stick_button){
-            if(!leftstickbuttonhasbeenpressed){
-                locked=!locked;
-                leftstickbuttonhasbeenpressed=true;
+    public static double kx=4;
+    public static double ky=4;
+    boolean useCombineServoControl = true;
+    ServoValueEasyOutputter.ClipPosition CurrentClipPosition = ServoValueEasyOutputter.ClipPosition.UNLOCKED;
+    boolean pad2_rbispressed = false;
+    boolean ifGivenCommand = false;
+    boolean pad2_lstickispressed = false;
+    boolean pad2_rstickispressed = false;
+    int servo_select = 1;
+    double[] servo_position = {0.5,0.5,0.5,0.5,0.5};//伺服位置数组
+    boolean p2xIsPressed = false;
+    public void servoControl() {
+        if(gamepad2.x){
+            if(p2xIsPressed){
+                p2xIsPressed = true;
+                useCombineServoControl =!useCombineServoControl;
             }
         }else{
-            leftstickbuttonhasbeenpressed=false;
+            p2xIsPressed = false;
         }
-        if(locked){
-            pos=ServoValueEasyOutputter.ClipPosition.LOCKED;
-        }else{
-            pos=ServoValueEasyOutputter.ClipPosition.UNLOCKED;
-        }
-        if(gamepad2.right_stick_button)
-            pos=ServoValueEasyOutputter.ClipPosition.HALF_LOCKED;
-        servoValueEasyOutputter.setClip(pos);
 
 
-        if(gamepad2.left_trigger > 0.1){
-            sixServoArmController.giveTheSample();
+        if(useCombineServoControl) {
+
+
+
+
+            //自动操作
+
+            x += gamepad2.left_stick_x * kx;
+            y -= gamepad2.left_stick_y * ky;
+            if (!gamepad2.dpad_left && !gamepad2.dpad_up && !gamepad2.dpad_right && !gamepad2.dpad_down) {
+                if (!gamepad2.left_bumper)
+                    sixServoArmController.setTargetPosition(x, y, Math.PI, clipRadian).update();
+                else {
+                    ArmAction Command = CVModule.CalculateAverage(CVModule);
+                    if (Command.suggestion != -2 && !HasSetArmPos) {
+                        x = Command.GoToX - 94;
+                        y = -Command.GoToY + 44;
+                        clipRadian = Command.ClipAngle;
+                        HasSetArmPos = true;
+                    }
+                }
+            }
+            if (gamepad2.dpad_left) {
+                sixServoArmController.prepareForTaking();
+            }
+            if (gamepad2.dpad_up) {
+                sixServoArmController.testIfTheSampleIsEaten();//todo 实现该功能
+            }
+            if (gamepad2.dpad_right) {
+                sixServoArmController.dropTheSample();
+            }
+            if (gamepad2.dpad_down) {
+                sixServoArmController.giveTheSample();
+            }else{
+                for(int i=0;i<=1;i++){
+                    sixServoArmController.giveTheSampleCheckPointInited[i]=false;
+                    sixServoArmController.giveTheSampleCheckPointPassed[i]=false;
+                }
+            }
+            ServoValueEasyOutputter.ClipPosition pos;
+            boolean changed=false;
+            if (gamepad2.left_stick_button) {
+                if (!leftstickbuttonhasbeenpressed) {
+                    locked = !locked;
+                    changed=true;
+                    leftstickbuttonhasbeenpressed = true;
+                }
+            } else {
+                leftstickbuttonhasbeenpressed = false;
+            }
+            if (gamepad2.right_stick_button) {
+                changed=true;
+            }
+            if(changed) {
+                if (locked) {
+                    pos = ServoValueEasyOutputter.ClipPosition.LOCKED;
+                } else {
+                    pos = ServoValueEasyOutputter.ClipPosition.UNLOCKED;
+                }
+                if(gamepad2.right_stick_button)pos = ServoValueEasyOutputter.ClipPosition.HALF_LOCKED;
+                servoValueEasyOutputter.setClip(pos);
+            }
+
+
+        }else {
+
+
+
+
+
+
+            //手动操作
+
+
+            if(gamepad2.left_stick_button) {
+                if (!pad2_lstickispressed) {
+                    servo_select--;
+                    pad2_lstickispressed = true;
+                    ifGivenCommand = false; // Reset command flag when changing servo selecti
+                }
+            } else {
+                pad2_lstickispressed = false;
+            }
+            if(gamepad2.right_stick_button) {
+                if (!pad2_rstickispressed) {
+                    servo_select++;
+                    pad2_rstickispressed = true;
+                    ifGivenCommand = false;
+                }
+            } else {
+                pad2_rstickispressed = false;
+            }
+            if (servo_select < 0) {
+                servo_select = 4;
+            }
+            if(servo_select > 4){
+                servo_select = 0;
+            }
+
+            if(gamepad2.left_stick_x > 0.1 && gamepad2.left_stick_x < 0.6) {
+                servo_position[servo_select] += 0.01;
+                ifGivenCommand = true;
+            }
+            else if(gamepad2.left_stick_x < -0.1 && gamepad2.left_stick_x > -0.6) {
+                servo_position[servo_select] -= 0.01;
+                ifGivenCommand = true;
+            }
+            if(gamepad2.left_stick_x > 0.6) {
+                servo_position[servo_select] += 0.08;
+                ifGivenCommand = true;
+            }
+            else if(gamepad2.left_stick_x <-0.6) {
+                servo_position[servo_select] -= 0.08;
+                ifGivenCommand = true;
+            }
+
+            if (servo_position[servo_select] > 1)
+                servo_position[servo_select] = 1;
+            if (servo_position[servo_select] < 0)
+                servo_position[servo_select] = 0;
+            if(ifGivenCommand){
+                servoValueEasyOutputter.SingleServoControl(servo_select, servo_position[servo_select]);
+            }
+            if (gamepad2.right_bumper) {
+                if (!pad2_rbispressed) {
+                    t = System.currentTimeMillis();
+                    pad2_rbispressed = true;
+                }
+                if (System.currentTimeMillis() - t > 1000) {
+                    CurrentClipPosition = ServoValueEasyOutputter.ClipPosition.HALF_LOCKED;
+                }
+            } else {
+                if (pad2_rbispressed) {
+                    if (System.currentTimeMillis() - t < 500) {
+                        if (CurrentClipPosition == ServoValueEasyOutputter.ClipPosition.LOCKED) {
+                            CurrentClipPosition = ServoValueEasyOutputter.ClipPosition.UNLOCKED;
+                        } else {
+                            CurrentClipPosition = ServoValueEasyOutputter.ClipPosition.LOCKED;
+                        }
+                    }
+                }
+
+                pad2_rbispressed = false;
+            }
+            servoValueEasyOutputter.setClip(CurrentClipPosition);
         }
+    for(int i=0;i<=4;i++){
+        servo_position[i]=servoValueEasyOutputter.servoNowDegree[i];
+    }
         //intakeLengthController.setIntakeTargetPosition(servoValueEasyOutputter.giveTheSample(servoValueEasyOutputter.InstallerLocationX,servoValueEasyOutputter.InstallerLocationY,servoValueEasyOutputter.InstallerLocationZ)).update();
+    }
+    InstallerController installerController;
+    OutputController outputController;
+    boolean outputInited = false;
+    public double OutputstartTime = 0;
+    public static double OutputTime = 300;
+    public void outputAndInstallerControl() {
+        if (gamepad1.b) {
+            installerController.BeamSpinner(false);
+        }
+        if (gamepad1.a) {
+            installerController.BeamSpinner(true);
+        }
+
+
+        if (gamepad2.a) {
+            installerController.setMode(RobotStates.INSTALL_RUNMODE.EATING);
+        }
+        if (gamepad2.y) {
+            outputInited = false;
+            installerController.Install();
+        }
+        installerController.run();
+
+        if (gamepad2.b) {
+            installerController.setMode(RobotStates.INSTALL_RUNMODE.BACKING);
+            if (!outputInited) {
+                OutputstartTime = System.currentTimeMillis();
+                outputInited = false;
+            }
+
+            if (System.currentTimeMillis() - OutputstartTime > OutputTime) {
+                CurrentClipPosition = ServoValueEasyOutputter.ClipPosition.UNLOCKED;
+                outputController.ArmUp();
+            }
+        }
     }
 }
